ef40cc038c89539f2e857c67ad5ca3b5190617b3
andmansim <91722324+andmansim@users.noreply.github.com>
2022-12-13 16:43:32 +0100

    ya

--- calculos.py
+++ calculos.py
@@ -2,15 +2,11 @@ from operaciones import*
 
 a, b, c, d = (10, 5, 0, "Hola")
 
-a, b, s = suma(a, b)
-b, d, r = resta(a, b)
-b, b, p = producto(b, b)
-a, c, d = division(a, c)
-
-print( "{} + {} = {}".format(a, b, s ) )
-
-print( "{} - {} = {}".format(b, d, r ) )
-
-print( "{} * {} = {}".format(b, b, p ) )
-
-print( "{} / {} = {}".format(a, c, d ) )
+a1, b1, s = suma(a, b)
+print( "{} + {} = {}".format(a1, b1, s ) )
+b1, d1, r = resta(a, b)
+print( "{} - {} = {}".format(b1, d1, r ) )
+b2, b2, p = producto(b, b)
+print( "{} * {} = {}".format(b2, b2, p ) )
+a2, c2, d = division(a, c)
+print( "{} / {} = {}".format(a2, c2, d ) )

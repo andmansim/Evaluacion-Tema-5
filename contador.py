c0f44eca9f6cfe80d2dbce909fcecd4b850b619e
andmansim <91722324+andmansim@users.noreply.github.com>
2022-12-13 17:00:23 +0100

    prueba

--- contador.py
+++ contador.py
@@ -12,4 +12,17 @@ def leer():
     contenido = leer.read()
     print(contenido)
     leer.close()
-    
+leer()
+
+def aniadir(inc = False, dec = False):
+    if inc:
+        suma = open('contador.txt', 'a')
+        suma.write('+1')
+        suma.close()
+        leer()
+    elif dec:
+        resta = open('contador.txt', 'a')
+        resta.write('-1')
+        resta.close()
+        leer()
+aniadir(True, False)

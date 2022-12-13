a7db90481fac245ff5609291253d8807aea29632
andmansim <91722324+andmansim@users.noreply.github.com>
2022-12-13 16:55:54 +0100

    asds

--- contador.py
+++ contador.py
@@ -1,14 +1,15 @@
 #Creamos archivo 
-try:
-    open('contador.txt')  
-except FileNotFoundError:     
-    contador = open('contador.txt', 'w')
-    contador.write('0')
-    contador.close()
-    
-#Leemos el archivo 
-leer = open('contador.txt', 'r') 
-contenido = leer.read()
-print(contenido)
-leer.close()
+def leer():
+    try:
+        open('contador.txt')  
+    except FileNotFoundError:     
+        contador = open('contador.txt', 'w')
+        contador.write('0')
+        contador.close()
+
+    #Leemos el archivo 
+    leer = open('contador.txt', 'r') 
+    contenido = leer.read()
+    print(contenido)
+    leer.close()
     

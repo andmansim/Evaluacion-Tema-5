bf421535be3ee4bcbdf492697c9c41a1b3f5bb22
andmansim <91722324+andmansim@users.noreply.github.com>
2022-12-13 16:52:05 +0100

    ya

--- contador.py
+++ contador.py
@@ -1,4 +1,7 @@
 #Creamos archivo 
-contador = open('contador.txt', 'w')
-contador.write('0')
-contador.close()
+try:
+    open('contador.txt')
+except FileNotFoundError:     
+    contador = open('contador.txt', 'w')
+    contador.write('0')
+    contador.close()
